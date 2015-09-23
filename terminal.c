c8a6ae2758575c4fb1102490b1254ac25a278d3c
Tyler Woodside <twoodside@loki.ist.unomaha.edu>
2015-09-23 01:02:14 -0500

    Separate commands

--- terminal.c
+++ terminal.c
@@ -4,38 +4,74 @@
 
 
 int main(char** argc, int argv) {
-    char *word;
-    char c = '\0';
-    int characterCounter = 10;
-    int status;
+  char **command=NULL;
+  char *word=NULL;
+  char c = '\0';
+  int wordCounter = 10;
+  int characterCounter = 10;
+  int stopCommand=0;
+  int stopWord=0;
+  int i;
+  int status;
 
-    while (word == NULL || strcmp(word, "exit") != 0) {
+  
+  
+  
+  while (command == NULL || strcmp(command[0], "exit") != 0) {
+    int maxCommandSize = wordCounter;
+    command = calloc(maxCommandSize,sizeof(char*));
+    wordCounter=0;
+    word=NULL;
+    
+    while ( word==NULL || !stopCommand ){
+      int maxSize = characterCounter;
+      word = calloc(maxSize, sizeof(char));
+      characterCounter = 0;
+      c = '\0';
 
-        int maxSize = characterCounter;
-        word = calloc(maxSize, sizeof(char));
-        characterCounter = 0;
-        c = '\0';
-
-        while (!(c == ' ' || c == '\t' || c == '\n' || c == EOF)) {
-            status = read(0, &c, 1);
-            
-            if (characterCounter==maxSize){
-                maxSize*=2;
-                word = realloc(word,maxSize*sizeof(char));
-            }
-            
-            word[characterCounter++] = c;
+      while (!stopWord) {
+        status = read(0, &c, 1);
+        
+        if (characterCounter==maxSize){
+          maxSize*=2;
+          word = realloc(word,maxSize*sizeof(char));
         }
         
-        word[characterCounter-1] = '\0';
+        word[characterCounter++] = c;
         
-        printf("%.*s\n",maxSize, word);
+        if (c == ' ' || c == '\t' || c == '\n' || c == EOF){
+          stopWord=1;
+        }
+      }
+      stopWord=0;
+      
+      if (word[characterCounter-1]=='\n'){
+        stopCommand=2;
+      }
+      
+      word[characterCounter-1] = '\0';
+      
+      if ( strcmp(word,";")==0 || strcmp(word,"&&")==0 || strcmp(word,"||")==0 ){
+        stopCommand=1;
+      }
+      
+      command[wordCounter++]=word;
+      
+      //printf("word: %s\n",word);
     }
+    stopCommand=0;
     
-    while (c!='\n'){
-      read(0,&c,1);
+    printf("command: ");
+    for (i=0;i<wordCounter;i++){
+      printf("%s ",command[i]);
     }
+    printf("\n");
+  }
+  
+  while (c!='\n'){
+    read(0,&c,1);
+  }
 
-    printf("FINISHED\n");
-    return 0;
+  printf("FINISHED\n");
+  return 0;
 }
