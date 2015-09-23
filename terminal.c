d8ece4bbc947cc8d5a36932531431b9ff9ec9dc8
Tyler Woodside <twoodside@loki.ist.unomaha.edu>
2015-09-23 11:16:43 -0500

    Checks whether command has /, then decides where to look for executable

--- terminal.c
+++ terminal.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct{
   char* str;
@@ -9,6 +10,7 @@ typedef struct{
 
 String readWord();
 String* readCommand();
+int searchForCommand(char *str);
 
 int main(char** argc, int argv) {
   String *command=NULL;
@@ -19,17 +21,79 @@ int main(char** argc, int argv) {
   int status;
   
   while (command == NULL || strcmp(command[0].str, "exit") != 0) {
+	pid_t process;
+	printf(">");
+	fflush(stdout);
     command = readCommand();
     
+	process = fork();
+	
+	if (process<0){
+		continue;
+	}
+	else if (process==0){
+		//This is the child
+		char c='\0';
+		printf("this is the child\n");
+		
+		if (command[0].str[0]=='/'){
+			int commandFound = searchForCommand(command[0].str);
+			printf( "%d\n",commandFound );
+		}
+		else{
+			char *newCommand = malloc(sizeof(char)*(strlen(command[0].str)+16)); //Something about this strcat fails
+			strcpy(newCommand,"/usr/local/bin/");
+			newCommand = strcat(newCommand,command[0].str);
+			
+			printf("%s\n",newCommand);
+			
+			int commandFound = searchForCommand(newCommand);
+			printf( "%d\n",commandFound );
+		}
+		
+		printf("c:");
+		
+		fflush(stdout);
+		
+		while (c!='\n'){
+			read(0, &c, 1);
+		}
+		
+		if (strcmp(command[0].str,"exit")==0){
+			printf("exiting with 0 status\n");
+			fflush(stdout);
+			exit(0);
+		}
+		else{
+			printf("exiting with 1 status\n");
+			fflush(stdout);
+			exit(1);
+		}
+	}
+	else{
+		//Parent
+		int status;
+		waitpid(process,&status,0); //This isn't waiting for the child process to exit?!
+		printf(">status: %d\n",status);
+	}
+	
+	
+	
+	/*
     printf("command: ");
     for (i=0;command[i].str!=NULL;i++){
       printf("%s ",command[i].str);
     }
     printf("\n");
+	*/
   }
   
   printf("FINISHED\n");
   return 0;
+}
+
+int searchForCommand(char* location){
+	return access(location,F_OK);
 }
 
 String* readCommand(){
